dc75aa030e46e92ba1a18d7f268e55a0b844f384
Lesya13 <pupochechka@mail.ru>
2021-08-22 17:20:59 +0300

    Homework code from lesson02

---
+++ src/test/java/ru/mytests/AppTest.java
@@ -0,0 +1,26 @@
+package ru.mytests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Test1");;
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        System.out.println("Test2");;
+    }
+}
---
+++ src/test/java/ru/mytests/IntegrationTest.java
@@ -0,0 +1,21 @@
+package ru.mytests;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+        /**
+         * Rigorous Test :-)
+         */
+        @Test
+        public void shouldAnswerWithTrue()
+        {
+            System.out.println("Integration Test1");;
+        }
+
+        @Test
+        public void shouldAnswerWithTrue2()
+        {
+            System.out.println("Integration Test2");;
+        }
+}
